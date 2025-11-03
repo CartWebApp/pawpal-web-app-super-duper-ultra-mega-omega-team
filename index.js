2f38beb9a55f82333462e9f15caf53f2dd09d02a
DevinKC261 <devin26chanthirath@gmail.com>
2025-11-03 14:55:20 -0800

    AAAAAAAAAAAAAAAA

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // Get a reference to the "Get Started" button
 const getStartedButton = document.querySelector('.start a');
-
-if (getStartedButton) {
-    // We already know the URL is relative to the Pet Selection directory
-    getStartedButton.href = "Pet Selection/pet-selection.html";
-}
-
-// The original script.js file had placeholder content. This replaces it with useful navigation logic.
